778b9bfb49972220de51f909d79e69310f9031be
revoiot <revowater@gmail.com>
2020-08-07 20:46:12 +0900

    no message

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -5,13 +5,11 @@ namespace App\Http\Controllers;
 use App\Folder;
 use App\Http\Requests\CreateFolder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FolderController extends Controller
 {
-    public function showCreateForm()
-    {
-        return view('folders/create');
-    }
+    // 中略
 
     public function create(CreateFolder $request)
     {
@@ -19,8 +17,9 @@ class FolderController extends Controller
         $folder = new Folder();
         // タイトルに入力値を代入する
         $folder->title = $request->title;
-        // インスタンスの状態をデータベースに書き込む
-        $folder->save();
+
+        // ★ ユーザーに紐づけて保存
+        Auth::user()->folders()->save($folder);
 
         return redirect()->route('tasks.index', [
             'id' => $folder->id,
